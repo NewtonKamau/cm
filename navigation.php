d3ec8185dc79200f7e75db77ecd41433a95755d3
NewtonKamau <kamaunewton78@gmail.com>
2020-11-20 16:09:15 +0300

    navigation files

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+
+<a href="home.php">Home Report </a>
+<a href="create_contact.php">Create contact</a>
+<a href="update.php">Update contact</a>
+<a href="delete.php">Delete</a>
+
+'
+?>
