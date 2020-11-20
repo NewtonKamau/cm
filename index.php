3048132a78576fc5b032831a9db9fe6b19fec28e
NewtonKamau <kamaunewton78@gmail.com>
2020-11-20 10:47:29 +0300

    insert multiple records

--- index.php
+++ index.php
@@ -11,10 +11,14 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 
 // insert data into the table
-$sql = "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'Steve','NDOE',012334566,'steve@doe.com','mwiki nairobi')";
-if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'Steve','NDOE',012334566,'steve@doe.com','mwiki nairobi');";
+$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'jonte','jonte',012334566,'mik@mike.com','mwiki nairobi');";
+$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'mike','mike',012334566,'jonte@doe.com','mwiki nairobi');";
+$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'mwas','mwas',012334566,'mwas@mawas.com','mwiki nairobi');";
+$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'kevo','kevo',012334566,'kevo@kevo.com','mwiki nairobi')";
+if ($conn->multi_query($sql) === TRUE) {
 	$last_id = $conn->insert_id;
-  echo "New record created successfully. Last id is ". $last_id;
+  echo "New records created successfully. Last id is ". $last_id;
 } else {
   echo "Error sql query: " .$sql. $conn->error;
 }
