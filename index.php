35f56ae3a0e643eb7f111d7b975573123313a787
NewtonKamau <kamaunewton78@gmail.com>
2020-11-20 11:01:40 +0300

    prepared statement for data entry

--- index.php
+++ index.php
@@ -10,19 +10,43 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 
 
-// insert data into the table
-$sql = "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'Steve','NDOE',012334566,'steve@doe.com','mwiki nairobi');";
-$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'jonte','jonte',012334566,'mik@mike.com','mwiki nairobi');";
-$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'mike','mike',012334566,'jonte@doe.com','mwiki nairobi');";
-$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'mwas','mwas',012334566,'mwas@mawas.com','mwiki nairobi');";
-$sql .= "INSERT INTO people(id ,name ,lastname ,telephone ,email ,address ) VALUES (NULL,'kevo','kevo',012334566,'kevo@kevo.com','mwiki nairobi')";
-if ($conn->multi_query($sql) === TRUE) {
-	$last_id = $conn->insert_id;
-  echo "New records created successfully. Last id is ". $last_id;
-} else {
-  echo "Error sql query: " .$sql. $conn->error;
-}
+// prepare and bind to help insert multiple data repeatedly
+$stm = $conn->prepare("INSERT INTO people(name,lastname,telephone,email,address) VALUES (?,?,?,?,?)");
+//binid the parameters with the query
+$stm->bind_param("sssss",$name,$lastname,$telephone,$email,$address);
 
+//set parameters and execute
+$name = "Kamahuha";
+$lastname = "Kamahuha";
+$telephone= 07123456;
+$email = "kamauhuha@gmail.com";
+$address = "runda nai";
+$stm->execute();
+
+$name = "Kamahuha";
+$lastname = "Kamahuha";
+$telephone= 07123456;
+$email = "kamauhuha@gmail.com";
+$address = "runda nai";
+$stm->execute();
+
+$name = "Kamahuha";
+$lastname = "Kamahuha";
+$telephone= 07123456;
+$email = "kamauhuha@gmail.com";
+$address = "runda nai";
+$stm->execute();
+
+$name = "Kamahuha";
+$lastname = "Kamahuha";
+$telephone= 07123456;
+$email = "kamauhuha@gmail.com";
+$address = "runda nai";
+$stm->execute();
+
+echo "New records created successfully";
+//close execution
+$stm->close();
 //close connection
 $conn->close();
 
